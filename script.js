dae9e8b60b91dd44e683daf85e36af1f420ce378
Pelicie <mecagoenlashormigas@gmail.com>
2024-12-24 00:10:14 -0500

    Agregando el slides y las fotos

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const swiper = new Swiper(".swiper", {
+  loop: true,
   pagination: {
     el: ".swiper-pagination",
   },
-  pagination: {
-    el: ".swiper-pagination",
+  scrollbar: {
+    el: '.swiper-scrollbar',
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  effect: 'cards',
 });
